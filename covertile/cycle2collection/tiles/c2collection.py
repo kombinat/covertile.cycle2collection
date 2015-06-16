53851bd57008d55df3ad0f42b0845b88e4b89440
Peter Mathis <peter.mathis@kombinat.at>
2015-06-16 08:48:20 +0200

    initial commit

---
+++ covertile/cycle2collection/tiles/c2collection.py
@@ -0,0 +1,109 @@
+'''
+Created on 16.6.2015
+
+@author: peterm
+'''
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from collective.cover.tiles.collection import ICollectionTile, CollectionTile
+from collective.cover.tiles.configuration_view import IDefaultConfigureForm
+from plone import api
+from plone.autoform import directives as form
+from zope import schema
+from zope.interface.declarations import implements
+
+from .widgets import TileTextLinesFieldWidget
+
+
+class ICycle2CollectionTile(ICollectionTile):
+    """ the same fields as Carousel tile """
+
+    form.omitted('columns')
+    form.no_omit(IDefaultConfigureForm, 'columns')
+    columns = schema.TextLine(
+        title=u"No. Columns",
+        required=False,
+    )
+
+    form.omitted('image_scale_dir')
+    form.no_omit(IDefaultConfigureForm, 'image_scale_dir')
+    image_scale_dir = schema.TextLine(
+        title=u"Image scale crop direction",
+        description=u"Can be 'thumbnail' (uncropped) or 'down' (cropped)",
+        required=False,
+        default=u'thumbnail',
+    )
+
+    form.omitted('timeout')
+    form.no_omit(IDefaultConfigureForm, 'timeout')
+    timeout = schema.TextLine(
+        title=u"Timeout between slides",
+        description=u"in milliseconds ... '0' means slider does not " \
+                    u"play automatically",
+        required=False,
+        default=u'0',
+    )
+
+    form.omitted('more_link')
+    form.no_omit(IDefaultConfigureForm, 'more_link')
+    form.order_after(more_link='image')
+    more_link = schema.Text(
+        title=u'More Link',
+        required=False,
+    )
+
+    form.widget(
+        columns=TileTextLinesFieldWidget,
+        image_scale_dir=TileTextLinesFieldWidget,
+        timeout=TileTextLinesFieldWidget,
+    )
+
+
+class Cycle2CollectionTile(CollectionTile):
+    implements(ICycle2CollectionTile)
+    index = ViewPageTemplateFile('c2collection.pt')
+    short_name = u'Cycle2 Collection'
+
+    def thumbnail(self, item):
+        """Return a thumbnail of an image if the item has an image field and
+        the field is visible in the tile.
+
+        :param item: [required]
+        :type item: content object
+        """
+        if self._has_image_field(item) and self._field_is_visible('image'):
+            tile_conf = self.get_tile_configuration()
+            image_conf = tile_conf.get('image', None)
+            if image_conf:
+                scaleconf = image_conf['imgsize']
+                # scale string is something like: 'mini 200:200'
+                # we need the name only: 'mini'
+                if scaleconf == '_original':
+                    scale = None
+                else:
+                    scale = scaleconf.split(' ')[0]
+                scales = item.restrictedTraverse('@@images')
+                crop_dir = tile_conf.get('image_scale_dir', {}).get('text') \
+                    or 'thumbnail'
+                return scales.scale('image', scale, direction=crop_dir)
+
+    @property
+    def columns(self):
+        try:
+            return int(self.get_tile_configuration().get(
+                'columns', {}).get('text'))
+        except:
+            pass
+        return 1
+
+    @property
+    def col_width(self):
+        grid_setting = api.portal.get_registry_record(
+            'collective.cover.controlpanel.ICoverSettings.grid_system')
+        if grid_setting == 'col12grid':
+            return int(12 / self.columns)
+        return int(16 / self.columns)
+
+    @property
+    def timeout(self):
+        return self.get_tile_configuration().get('timeout', {}).get(
+            'text', '0')
